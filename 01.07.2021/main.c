d469c58635df2090b5d086ce6f99f4156355e2e4
222010404054 <83009123+ihatemyself02@users.noreply.github.com>
2021-07-03 13:47:05 +0000

    scan_ex.c

--- 01.07.2021/main.c
+++ 01.07.2021/main.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
 
-int main(void) {
-  printf("Go Tigers!!\n");
-  return 0;
- }
 #include <stdio.h>
 
 int main(){
@@ -11,4 +6,5 @@ int main(){
    i1=printf("[%5d] [%+05d] [%#5o] [%#7x]\n", 123, 1234, 123, 123);
    i2=printf("[%f] [%5.2f] [%+0.5.0f]\n", 123.456, 123.456, 123.456);
    printf(%d, %d, i1 )
+   return 0;
 }
---
+++ 01.07.2021/scan_ex.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+  int age=0, i1, i2;
+  printf("Enter age: ");
+  i1scanf("%d", &age);
+  printf("Your age  is %d",age);
+  return 0;
+}
