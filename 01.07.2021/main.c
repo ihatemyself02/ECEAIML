dd07925b97e3913077a1bad0c0d82f3143046bca
222010404054 <83009123+ihatemyself02@users.noreply.github.com>
2021-07-03 13:34:57 +0000

    main

--- 01.07.2021/main.c
+++ 01.07.2021/main.c
@@ -4,3 +4,11 @@ int main(void) {
   printf("Go Tigers!!\n");
   return 0;
  }
+#include <stdio.h>
+
+int main(){
+   int i1,i2;
+   i1=printf("[%5d] [%+05d] [%#5o] [%#7x]\n", 123, 1234, 123, 123);
+   i2=printf("[%f] [%5.2f] [%+0.5.0f]\n", 123.456, 123.456, 123.456);
+   printf(%d, %d, i1 )
+}
